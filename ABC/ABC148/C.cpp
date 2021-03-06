925a2ce2fb5ea2cd186058b3820e6f28bf00db0e
moyac1012 <yuto.tamori@meetasu.jp>
2021-03-06 19:31:48 +0900

    2021/3/5

---
+++ ABC/ABC148/C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+ll gcd(ll a, ll b){
+    if(a < b) swap(a, b);
+    if(b == 0) return a;
+    return gcd(b , a%b);
+}
+ll lcm(ll a, ll b){
+    return a / gcd(a, b) * b;
+}
+int main(){
+    ll a, b;
+    cin >> a >> b;
+    cout << lcm(a, b) << endl;
+    return 0;
+}

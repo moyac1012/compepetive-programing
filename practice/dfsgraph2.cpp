feecd1c43944c6d338685a69b7863467439e597a
moyac1012 <yuto.tamori@meetasu.jp>
2021-03-23 17:53:32 +0900

    2020/3/23

---
+++ practice/dfsgraph2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+vector<bool> seen;
+void dfs(const Graph &G, int v){
+    seen[v] = true;
+    for(auto next_v : G[v]){
+        if(seen[next_v]) continue;
+        dfs(G, next_v);
+    }
+}
+int main(){
+    int n,m; cin >> n >> m;
+    Graph G(n);
+    rep(i,m){
+        int a,b; cin >> a >> b;
+        a--; b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    seen.assign(n, false);
+    int cnt = 0;
+    rep(v,n){
+        if(seen[v]) continue;
+        dfs(G, v);
+        cnt++;
+    }
+    cout << cnt-1 << endl;
+    return 0;
+}
--- practice/dfsgrid3.cpp
+++ practice/dfsgrid3.cpp
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 #define rep(i,n) for(int i = 0; i < (int)(n); i++)
-const int dx[4] = {1,0,-1,0};
-const int dy[4] = {0,1,0,-1};
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
 int h,w;
 vector<string> maze;
 
 bool seen[510][510] = {};
 void dfs(int sh, int sw){
-    seen[sh][sw] = 1;
+    seen[sh][sw] = true;
     rep(i,4){
         int nh = sh + dx[i], nw = sw + dy[i];
-        if(nh < 0 || nw < 0 || nh >= h || nw >= w) continue;
-        if(maze[nh][nw] == '#') continue;
-        if(seen[nh][nw] == 1) continue;
-
+        if(nh < 0 || nw < 0 || nh >= h || nw >= w || seen[nh][nw] == true || maze[nh][nw] == '#') continue;
         dfs(nh, nw);
     }
 }
-
 int main(){
     cin >> h >> w;
     maze.resize(h);
     rep(i,h) cin >> maze[i];
-    int sh, sw, gh, gw;
+    int sh,sw,gh,gw;
     rep(i,h)rep(j,w){
         if(maze[i][j] == 's') sh = i, sw = j;
         if(maze[i][j] == 'g') gh = i, gw = j;
     }
     dfs(sh, sw);
-    if(seen[gh][gw] == 1) cout << "Yes" << endl;
+    if(seen[gh][gw]) cout << "Yes" << endl;
     else cout << "No" << endl;
     return 0;
 }

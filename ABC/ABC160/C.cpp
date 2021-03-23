9d059c44561b32b51cc8e4022e7cd6e1ed3f9624
moyac1012 <yuto.tamori@meetasu.jp>
2021-03-23 17:35:08 +0900

    2021/3/23

--- ABC/ABC160/C.cpp
+++ ABC/ABC160/C.cpp
@@ -11,6 +11,17 @@ int main()
     cin >> k >> n;
     vector<int> a(n);
     rep(i,n) cin >> a[i];
-    
+    int ans = 1e7;
+    rep(i,n){
+        if(i == 0){
+            ans = min(a[n-1] - a[i], (k-a[i+1])+a[i]);
+        }else if(i == n-1){
+            ans = min(ans, min( (k-a[i])+a[i-1], a[i]-a[0] ));
+        }else{
+            ans = min(min( (k-a[i])+a[i-1], (k-a[i+1])+a[i] ),ans);
+        }
+        //cout << ans << endl;
+    }
+    cout << ans << endl;
     return 0;
 }

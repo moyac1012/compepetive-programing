6944f1defee993336cd2cb79360e259074e834be
moyac1012 <yuto.tamori@meetasu.jp>
2021-03-24 01:47:02 +0900

    2020/3/24

--- ABC/ABC150/C.cpp
+++ ABC/ABC150/C.cpp
@@ -1,37 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
-ll Fact(ll n){
-    ll ans = 1;
-    for (size_t i = 1; i <= n; i++)
-    {
-        ans *= i;
-    }
-    return ans;
-}
-int main()
-{
-    int N;
-    cin >> N;
-    vector<int> P(N),Q(N);
-    rep(i,N) cin >> P[i];
-    rep(i,N) cin >> Q[i];
-    vector<vector<int>> perm(N,vector<int>(Fact(N)));
-    vector<int> v(N);
-    iota(v.begin(), v.end(),1);
-    int j = 0;
-    do{
-        for (size_t i = 0; i < N; i++)
-        {
-            perm[i][j] = v[i];
-            //cout << perm[i][j] << " ";
-        }
-        //cout << endl;
-        j++;
-    }while(next_permutation(v.begin(),v.end()));
-    int Pt=0,Qt=0;
-    
-    cout << abs(Pt-Qt) << endl;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
 
+int main(){
+    int n; cin >> n;
+    vector<int> p(n),q(n);
+    rep(i,n) cin >> p[i];
+    rep(i,n) cin >> q[i];
+    rep(i,n){
+        p[i]--; q[i]--;
+    }
+    int cntp = 0, cntq= 0;
+    vector<int> a(n);
+    rep(i,n) a[i] = i;
+    while(a != p){
+        next_permutation(a.begin(), a.end());
+        cntp++;
+    }
+    rep(i,n) a[i] = i;
+    while(a != q){
+        next_permutation(a.begin(), a.end());
+        cntq++;
+    }
+    cout << abs(cntp - cntq) << endl;
+    return 0;
 }

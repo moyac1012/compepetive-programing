e8d24c0456052541c264371059048c42038c43af
moyac1012 <yuto.tamori@meetasu.jp>
2021-06-20 19:32:08 +0900

    2021/6/20

---
+++  典型90問/027.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+
+int main(){
+    int n; cin >> n;
+    set<string> p;
+    int cnt = 0;
+    rep(i,n){
+        string s; cin >> s;
+        p.insert(s);
+        if(p.size() > cnt){
+            cnt++;
+            cout << i+1 << endl;
+        }
+    }
+    return 0;
+}

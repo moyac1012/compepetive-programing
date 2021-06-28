8365bc1ec56d1aeaef63174df65a77ca6ec3a61f
moyac1012 <yuto.tamori@meetasu.jp>
2021-06-28 15:19:08 +0900

    2021/6/28

--- ABC/ABC097/C.cpp
+++ ABC/ABC097/C.cpp
@@ -10,32 +10,19 @@ template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout 
 template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
 template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
 template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
-int powij(int i, int j){
-    int ans = 1;
-    rep(a,j){
-        ans *= i;
-    }
-    return ans;
-}
+
 int main(){
-    int x;
-    cin >> x;
-    vector<bool> flag(1005);
-    vector<int> a;
-    a.push_back(1);
-    for(int i = 2; i <= 1005; i++){
-        int j = 2;
-        while(powij(i,j) <= 1005 && flag[powij(i,j)] == false){
-            a.push_back(powij(i,j));
-            flag[powij(i,j)] = true;
-            j++;
+    string s; cin >> s;
+    int k; cin >> k;
+    set<string> st;
+    for(int i = 0; i < s.length(); i++){
+        for(int j = 1; j <= s.length() - i; j++){
+            st.insert(s.substr(i,j));
         }
     }
-    sort(a.begin(), a.end());
-    int ans = 1;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] <= x) ans = max(ans, a[i]);
-    }
-    cout << ans << endl;
+    vector<string> stv;
+    for(auto i = st.begin(); i != st.end(); i++) stv.push_back(*i);
+    sort(stv.begin(), stv.end());
+    cout << stv[k-1] << endl;
     return 0;
 }
--- ABC/ABC097/D.cpp
+++ ABC/ABC097/D.cpp
@@ -10,32 +10,47 @@ template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout 
 template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
 template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
 template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
-int powij(int i, int j){
-    int ans = 1;
-    rep(a,j){
-        ans *= i;
-    }
-    return ans;
-}
+struct UnionFind {
+   vector<int> par;
+   UnionFind(int N) : par(N) {
+      for(int i = 0; i < N; i++) par[i] = i;
+   }
+
+   int root(int x) {
+      if (par[x] == x) return x;
+      return par[x] = root(par[x]);
+   }
+
+   void unite(int x, int y) {
+      int rx = root(x);
+      int ry = root(y);
+      if (rx == ry) return;
+      par[rx] = ry;
+   }
+
+   bool same(int x, int y) {
+      int rx = root(x);
+      int ry = root(y);
+      return rx == ry;
+   }
+};
+
 int main(){
-    int x;
-    cin >> x;
-    vector<bool> flag(1005);
-    vector<int> a;
-    a.push_back(1);
-    for(int i = 2; i <= 1005; i++){
-        int j = 2;
-        while(powij(i,j) <= 1005 && flag[powij(i,j)] == false){
-            a.push_back(powij(i,j));
-            flag[powij(i,j)] = true;
-            j++;
-        }
+    int n,m; cin >> n >> m;
+    vector<int> p(n);
+    UnionFind tree(n);
+    rep(i,n) {
+        cin >> p[i];
+        p[i]--;
     }
-    sort(a.begin(), a.end());
-    int ans = 1;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] <= x) ans = max(ans, a[i]);
+    rep(i,m){
+        int a,b;
+        cin >> a >> b;
+        a--; b--;
+        tree.unite(a,b);
     }
+    int ans = 0;
+    rep(i,n) if(tree.same(i, p[i])) ans++;
     cout << ans << endl;
     return 0;
 }

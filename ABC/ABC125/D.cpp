518c971cd18b666a1b765aeeb37fb14fc2313a2c
moyac1012 <yuto.tamori@meetasu.jp>
2021-07-01 00:05:33 +0900

    2021/6/30

--- ABC/ABC125/D.cpp
+++ ABC/ABC125/D.cpp
@@ -10,11 +10,25 @@ template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout 
 template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
 template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
 template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
-
+BIT
 int main(){
-    int a,b,t;
-    cin >> a >> b >> t;
-    int n = t/a;
-    cout << n*b << endl;
+    int n; cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    int cnt = 0;
+    rep(i,n) if(a[i] < 0) cnt++;
+    ll ans = 0, m = 1e9+5;
+    rep(i,n) {
+        a[i] = abs(a[i]);
+        ans += a[i];
+        m = min(a[i],m);
+    }
+    //cout<< ans << endl;
+    //cout << m << endl;
+    if(cnt%2 == 0){
+        cout << ans << endl;
+    }else{
+        cout << ans - 2*m << endl;
+    }
     return 0;
 }

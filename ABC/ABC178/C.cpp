c5743ae5e78542009910929f8d3b5673f25e8b6d
moyac1012 <yuto.tamori@meetasu.jp>
2021-07-02 15:14:40 +0900

    2021/7/1

--- ABC/ABC178/C.cpp
+++ ABC/ABC178/C.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
-ll GetDigit(ll num){ return log10(num)+1; } //numの桁数を求める
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+ll MOD = 1e9+7;
 
-int main()
-{
-    ll n;
-    cin >> n;
-    ll m =1e9+7;
+int main(){
+    ll n; cin >> n;
+    ll ans = modPow(10LL, n, MOD) - modPow(9LL, n, MOD) - modPow(9LL, n, MOD) + modPow(8LL, n, MOD);
+    ans %= MOD;
+    ans=(ans+MOD)%MOD;
+    cout << ans << endl;
 
-    if(n == 1){
-        cout << 0 << endl;
-        return 0;
-    }
-    ll ans = n*(n-1)%m;
-    rep(i,n-2){
-        ans *= 10%m;
-        ans %= m;
-    }
-    cout << ans%m << endl;
     return 0;
 }

766e905355365f19fcacac1ef3ae0d3124cf0837
moyac1012 <yuto.tamori@meetasu.jp>
2021-07-07 14:39:32 +0900

    kaisetu AC

---
+++ ABC/ABC023/D.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+int n;
+vector<ll> s,h;
+bool solve(ll mid){
+    vector<ll> lim(n);
+    rep(i,n) {
+        if(h[i] > mid) return false;
+        lim[i] = (mid - h[i])/s[i];
+    }
+    sort(lim.begin(), lim.end());
+    rep(i,n) if(lim[i] < i) return false;
+    return true;
+}
+
+int main(){
+    cin >> n;
+    s.assign(n,0);
+    h.assign(n,0);
+    rep(i,n) cin >> h[i] >> s[i];
+    ll left = -1, right = 1LL<<60;
+    while(left + 1 < right){
+        ll mid = left + (right - left)/2;
+        if(solve(mid)) right = mid;
+        else left = mid;
+    }
+    cout << right << endl;
+    return 0;
+}

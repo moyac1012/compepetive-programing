4e487e9138e863f9f582f972a6e6b818118a152c
moyac1012 <yuto.tamori@meetasu.jp>
2021-07-15 14:15:20 +0900

    echo 20210715_141520

--- ABC/ABC159/D.cpp
+++ ABC/ABC159/D.cpp
@@ -1,50 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-using ll = long long;
-ll GetDigit(ll num){ return log10(num)+1; } //numの桁数を求める
-uint64_t combinations2(uint64_t n, uint64_t k) {
-    uint64_t r = 1;
-    for (uint64_t d = 1; d <= k; ++d) {
-        r *= n--;
-        r /= d;
+using ll = unsigned long long;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
+
+int main(){
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    vector<ll> cnt(300000);
+    rep(i,n) cnt[a[i]]++;
+    ll total = 0;
+    vector<ll> c(300000), cd(300000);
+    rep(i,300000){
+        c[i] = cnt[i]*(cnt[i]-1)/2;
+        total += c[i];
     }
-    return r;
-}
-int main()
-{
-    ll N;
-    cin >> N;
-    vector<int> A(N);
-    rep(i,N) cin >> A[N];
-    vector<int> num(N);
-    rep(i,N){
-        num[A[i]]++;
-    }
-    rep(i,N){
-        cout << num[i] << " ";
-    }
-    cout << endl;
-    vector<int> conb1(N);
-    vector<int> conb2(N);
-    rep(i,N){
-        if(num[i] != 0) {
-            conb1[i] = combinations2(num[i],2);
-            conb2[i] = combinations2(num[i]-1,2);
-        }
-    }
-    rep(i,N){
-        cout << conb1[i] << " ";
-    }
-    cout << endl;
-    vector<int> ans(N);
-    rep(i,N){
-        ans[i] += conb2[A[i]];
-        rep(j,N){
-            if(i != j) ans[i] += conb1[j];
-        }
-    }
-    rep(i,N){
-        cout << ans[i] << endl;
-    }
+    rep(i,300000) cd[i] = (cnt[i]-1)*(cnt[i]-2)/2;
+    rep(i,300000) c[i] -= cd[i];
+    rep(i,n) cout << total - c[a[i]] << endl;
+    
+    return 0;
 }

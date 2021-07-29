a38ba260a568dc57dc3ebf2e9c1d1d5068914c8b
moyac1012 <yuto.tamori@meetasu.jp>
2021-07-29 23:05:38 +0900

    20210729_230538

--- ABC/ABC180/D.cpp
+++ ABC/ABC180/D.cpp
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
-ll GetDigit(ll num){ return log10(num)+1; } //numの桁数を求める
+using ull = unsigned long long;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
 
-int main()
-{
-    ll x, y, a, b;
-    cin >> x >> y >> a >> b;
-    ll cnt = 0;
-    
-    while(x*(a-1) <= b){
+int main(){
+    ull x,y,a,b; cin >> x >> y >> a >> b;
+    ull ans = 0;
+    while(a*x <= x+b && a*x < y && (double)a*x<=2e18){
+        ans++;
         x *= a;
-        cnt++;
-        if(x >= y){
-            cout << cnt-1 << endl;
-            return 0; 
-        }
     }
-    if((y-x)/b > 0){
-        cnt += (y-x)/b;
-        if((y-x)%b == 0) cnt--;
-    }
-
-    cout << cnt << endl;
+    ull c = (y-x)/b;
+    ans += c;
+    if((y-x)%b == 0) cout << ans-1 << endl;
+    else cout << ans << endl;
     return 0;
 }

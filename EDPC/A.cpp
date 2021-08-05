b34d93d8ad31e035fa2aee7abc5c8c50fd00dff1
moyac1012 <yuto.tamori@meetasu.jp>
2021-08-05 11:20:36 +0900

    20210805_112036

--- EDPC/A.cpp
+++ EDPC/A.cpp
@@ -1,22 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i,n) for(int i = 0; i < (int)(n); i++)
 #define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
 using Graph = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
 
 int main(){
     int n; cin >> n;
-    vector<int> h(n);
-    rep(i,n) cin >> h[i];
-    vector<int> dp(n+1);
-    dp[0] = 0;
-    dp[1] = abs(h[0]-h[1]);
-    Rep(i, 2, n){
-        dp[i] = min(dp[i-2]+abs(h[i-2]-h[i]), dp[i-1]+abs(h[i-1]-h[i]));
-    }
-    cout << dp[n-1] << endl;
+    vector<int> h(n+1);
+    for(int i = 1; i <= n; i++) cin >> h[i];
+    vector<int> dp(1000005, 100001);
+    dp[1] = 0;
+    dp[2] = abs(h[1] - h[2]);
+    Rep(i,3,n+1) dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]), dp[i-2] + abs(h[i] - h[i-2]));
+    cout << dp[n] << endl;
+    
     return 0;
 }
--- EDPC/B.cpp
+++ EDPC/B.cpp
@@ -1,24 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i,n) for(int i = 0; i < (int)(n); i++)
 #define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
 using Graph = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
 
 int main(){
     int n,k; cin >> n >> k;
-    vector<ll> h(n);
-    rep(i,n) cin >> h[i];
-    vector<ll> dp(100050, 1e18);
-    dp[0] = 0;
-    Rep(i,1,n){
-        for(int j = 1; j <= k; j++){
-            if(i - j < 0) break;
-            dp[i] = min(dp[i], dp[i-j] + abs(h[i-j]-h[i]));
+    vector<int> h(n+1);
+    Rep(i,1,n+1) cin >> h[i];
+    vector<int> dp(n+1, 1001001001);
+    dp[1] = 0;
+    for(int i = 1; i <= n; i++){
+        for(int j = i+1; j <= i+k; j++){
+            if(j <= n) dp[j] = min(dp[j], dp[i] + abs(h[i] - h[j]));
         }
     }
-    cout << dp[n-1] << endl;
+    cout << dp[n] << endl;
     return 0;
 }

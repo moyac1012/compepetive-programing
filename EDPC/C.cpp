6e95c174d477dd0302fced4d2642c6ebc3fe37f3
moyac1012 <yuto.tamori@meetasu.jp>
2021-08-05 11:32:45 +0900

    20210805_113245

--- EDPC/C.cpp
+++ EDPC/C.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i,n) for(int i = 0; i < (int)(n); i++)
 #define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
 using Graph = vector<vector<int>>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
 
 int main(){
     int n; cin >> n;
-    vector<vector<int>> a(n, vector<int>(3));
-    rep(i,n) cin >> a[i][0] >> a[i][1] >> a[i][2];
-    vector<vector<int>> dp(100050, vector<int>(3));
-    dp[0][0] = a[0][0];
-    dp[0][1] = a[0][1];
-    dp[0][2] = a[0][2];
-    Rep(i,1,n){
+    vvi v(n+1, vi(3));
+    Rep(i,1,n+1)rep(j,3) cin >> v[i][j];
+    vvi dp(n+1, vi(3));
+    rep(i,3) dp[1][i] = v[0][i];
+    Rep(i,2,n+1){
         rep(j,3){
-            dp[i][j] = max(dp[i-1][(j+1)%3]+ a[i][j],dp[i-1][(j+2)%3]+ a[i][j]);
+            dp[i][j] = max(v[i-1][(j+1)%3] + dp[i-1][j], v[i-1][(j+2)%3] + dp[i-1][j]);
         }
     }
-    int ans = 0;
-    rep(i,3){
-        ans = max(ans, dp[n-1][i]);
-    }
-    cout << ans << endl;
+    cout << max({dp[n][0], dp[n][1], dp[n][2]}) << endl;
     return 0;
 }

d6f91b7f99f311867b15b0f31ee7b6665ea819c3
moyac1012 <yuto.tamori@meetasu.jp>
2021-08-06 15:43:01 +0900

    20210806_154301

--- EDPC/D.cpp
+++ EDPC/D.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i,n) for(int i = 0; i < (int)(n); i++)
 #define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
 using Graph = vector<vector<int>>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
 
 int main(){
-    ll n, w0;
-    cin >> n >> w0;
-    vector<ll> w(n+1), v(n+1);
-    rep(i,n) cin >> w[i+1] >> v[i+1];
-    vector<vector<ll>> dp(110, vector<ll>(100100));
-    Rep(i,1,n+1)rep(j,w0+1){
-        if(j - w[i] >= 0) dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]);
-        else dp[i][j] = dp[i-1][j];
+    ll n,wm;
+    cin >> n >> wm;
+    vector<ll> v(n+1), w(n+1);
+    Rep(i,1,n+1) cin >> w[i] >> v[i];
+    vector<vector<ll>> dp(n+1, vector<ll>(wm+1));
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= wm; j++){
+            if(j - w[i] >= 0) dp[i][j] = max(dp[i-1][j], dp[i-1][j - w[i]] + v[i]);
+            else dp[i][j] = dp[i-1][j];
+        }
     }
-    // rep(i,n+1){
-    //     rep(j,w0+1){
-    //         cout << dp[i][j] << "\t";
-    //     }
-    //     cout << endl;
-    // }
-    cout << dp[n][w0] << endl;
+    cout << dp[n][wm] << endl;
     return 0;
 }

0cb05e8b302bba1dfb1eb68caa4cf861a250fa85
moyac1012 <yuto.tamori@meetasu.jp>
2022-05-13 20:14:08 +0900

    20220513_201408

---
+++ ABC/ABC220/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
+ll GetDigit(ll num){
+    ll digit=0;
+    while(num!=0){
+        num /= 10;
+        digit++;
+    }
+    return digit;
+}
+
+ll kto10(ll a, ll k){
+    ll ans = 0;
+    ll n = GetDigit(a);
+    rep(i,n){
+        ll keta1 = a%10;
+        ans += keta1*pow(k,i);
+        a /= 10;
+    }
+    return ans;
+}
+int main(){
+    ll k; cin >> k;
+    ll a,b; cin >> a >> b;
+    cout << kto10(a,k)*kto10(b,k) << endl;
+    return 0;
+}
---
+++ ABC/ABC220/D.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
+ll MOD = 998244353;
+int main(){
+    int n; cin >> n;
+    vector<int> a(n+1);
+    Rep(i,1,n+1) cin >> a[i];
+    ll dp[n+1][10];
+    rep(i,n+1)rep(j,10) dp[i][j] = 0;
+    dp[1][a[1]] = 1;
+    Rep(i,2,n+1){
+        rep(j,10){
+            dp[i][(j+a[i])%10] += dp[i-1][j]%MOD;
+            dp[i][(j*a[i])%10] += dp[i-1][j]%MOD;
+        }
+    }
+    rep(i,10) cout << dp[n][i]%MOD << endl;
+
+    return 0;
+}

0898c5375e92f281cf8e2ab5066a454f6aa6a2f9
moyac1012 <moyac@realidea.jp>
2023-07-21 20:11:29 +0900

    20230721_201129

--- abcnanika/b188.cpp
+++ abcnanika/b188.cpp
@@ -3,30 +3,30 @@ using namespace std;
 using ll = long long;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i,n) for(ll i = 0; i < (ll)(n); i++)
-#define Rep(i,j,n) for(ll i = j; i < (ll)(n); i++)
-using Graph = vector<vector<ll>>;
-using P = pair<ll, ll>;
-const ll dx[4] = {1, 0, -1, 0};
-const ll dy[4] = {0, 1, 0, -1};
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
 const char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-template<typename T_n> void prll_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
 template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
 template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
 template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
 //mod mにおけるaの逆元を計算する
 template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
-ll a[100009], b[100009];
-ll dp[100009];
+
 int main(){
-    ll n; cin >> n;
-    Rep(i,1,n) cin >> a[i];
-    Rep(i,1,n) cin >> b[i];
-    for(ll i = 1; i <= n; i++) dp[i] = -1000000;
-    for(ll i = 1; i <= n-1; i++){
-        dp[a[i]] = max(dp[a[i]], dp[i]+100);
-        dp[b[i]] = max(dp[b[i]], dp[i]+150);
+    int n; cin >> n;
+    vector<ll> a(n), b(n);
+    rep(i,n) cin >> a[i];
+    rep(i,n) cin >> b[i];
+    ll ans = 0;
+    rep(i,n){
+        ans += a[i]*b[i];
     }
-    cout << dp[n] << endl;
+    if(ans == 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
     return 0;
 }
---
+++ abcnanika/c188.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
+
+int main(){
+    int n; cin >> n;
+    vector<int> a(pow(2,n));
+    rep(i,pow(2,n)) cin >> a[i];
+    vector<int> c = a;
+    int l = pow(2,n);
+    int now = 0;
+    vector<int> b;
+    while(l > 2){
+        if(now >= l){
+            l /= 2;
+            a = b;
+            b.clear();
+            now = 0;
+        }
+
+        if(a[now] > a[now+1]){
+            b.push_back(a[now]);
+        }else{
+            b.push_back(a[now+1]);
+        }
+        now+=2;
+    }
+    int ans = min(a[0], a[1]);
+    rep(i,pow(2,n)){
+        if(c[i] == ans) cout << i+1 << endl;
+    }
+    
+    return 0;
+}

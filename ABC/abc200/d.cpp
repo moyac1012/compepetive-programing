4018b02d039d43417363e87b82b0fba2e5713188
moyac1012 <moyac@realidea.jp>
2023-07-21 22:05:07 +0900

    20230721_220507

---
+++ ABC/abc200/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
+
+int main(){
+    int n; cin >> n;
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    n = min(n, 8);
+    vector<vector<ll>> ans(201);
+    for(int bit = 1; bit < (1<<n); bit++){
+        vector<ll> s;
+        ll cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(bit & (1<<i)){
+                cnt += a[i]%200;
+                s.push_back(i+1);
+            }
+        }
+        if(ans[cnt%200].size() == 0){
+            ans[cnt%200] = s;
+        }else{
+            cout << "Yes" << endl;
+            cout << s.size() << " ";
+            print_vec(s);
+            cout << ans[cnt%200].size() << " ";
+            print_vec(ans[cnt%200]);
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}

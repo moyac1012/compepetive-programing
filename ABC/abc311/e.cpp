9747689d12c83ba279078ae2a63e6dd50e015361
moyac1012 <moyac@realidea.jp>
2023-07-24 08:55:03 +0900

    20230724_085503

---
+++ ABC/abc311/e.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define Rep(i,j,n) for(int i = j; i < (int)(n); i++)
+using Graph = vector<vector<int>>;
+using P = pair<int, int>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+template<typename T_n> void print_vec(vector<T_n> vec) { rep(i,vec.size()) cout << vec[i] << ' '; cout << endl; }
+template<typename T_n>T_n gcd(T_n a,T_n b){ if(a < b) swap(a, b); if(b == 0) return a; return gcd(b,a%b); }
+template<typename T_n> T_n lcm(T_n a, T_n b){ return (a/gcd(a, b))*b; }
+template<typename T_n> T_n modPow(T_n a, T_n n, T_n p){ if (n == 0) return 1; if (n == 1) return a % p; if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p; ll t = modPow(a, n / 2, p); return (t * t) % p; }
+//mod mにおけるaの逆元を計算する
+template<typename T_n> T_n modinv(T_n a, T_n m) { T_n b = m, u = 1, v = 0; while (b) { T_n t = a / b; a -= t * b; swap(a, b); u -= t * v; swap(u, v); } u %= m; if (u < 0) u += m; return u; }
+
+int main(){
+    int h,w,n; cin >> h >> w >> n;
+    vector<vector<bool>> hole(h+1, vector<bool>(w+1, false));
+    for(int i = 0; i < n; i++){
+        int a,b;
+        cin >> a >> b;
+        hole[a][b] = true;
+    }
+    vector<vector<int>> dp(h+1, vector<int>(w+1));
+    ll ans = 0;
+    Rep(i,1,h+1)Rep(j,1,w+1){
+        if(hole[i][j]) continue;
+        dp[i][j] = min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]})+1;
+        ans += dp[i][j];
+    }
+    cout << ans << endl;
+    return 0;
+}
